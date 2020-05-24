42685ecaf9ebaa88e99baa6a2d8da05b7c09e8e9
MehaG1 <64508470+MehaG1@users.noreply.github.com>
2020-05-24 09:28:53 -0700

    Fixed popup

--- popup.js
+++ popup.js
@@ -1,11 +1,12 @@
-class Schedule {
+//class Schedule {
 
-  constructor() {
+function mainB() {  
     var time = prompt("How much time do you want to be working to the nearest minute?");
-    var sched = planWork();
-  }
+    //var sched = planWork();
+  
+  
 
-  planWork() {
+  //function planWork() {
   var arr = [];
   var index = 0;
   while (this.time > 0) {
@@ -15,45 +16,51 @@ class Schedule {
       index++;
     }
     else if (time - 30 < 0 && index % 2 == 0) {
-      
       arr[arr.length] = time;
       time = 0;
       index++;
     }
+
     else {
       arr[arr.length] = 10;
       index++;
     }
   }
+  
   if (arr.length % 2 == 0) {
     arr.pop();
   }
-  return arr;
-}
+  //return arr;
+//}
 
-get sched()
-{
-  return this.sched;
-}
+//function sched()
+//{
+ // return this.sched;
+//}
 
-  mainB(){
-    var s = new Schedule();
-    var work = s.sched; // this is essentially planWork()
+//function mainB(){
+    //var s = new Schedule();
+    //var work = s.sched; // this is essentially planWork()
     var i = 0; // for for loop
     var total = 0;
 
     var str = "This is your schedule breakdown:";
-    for (i = 0; i < work.length; i++) {
+    for (i = 0; i < arr.length; i++) {
       if (i % 2 == 0) 
       { 
-        str += ("\nWork Period of " + wor[i] + " minutes.");   
+        str += ("\nWork Period of " + arr[i] + " minutes.");   
       }
       if (i % 2 != 0) {
-        str+=("\nBreak Period of " + work[i] + " minutes.");
+        str+=("\nBreak Period of " + arr[i] + " minutes.");
         total += 10;
       }
     }
-    str+=("\nYour recommended schedule will take an extra " + total + " minutes in breaktime.");
-    document.getElementById("checkpage").innerHTML = str;
-  }
+    str+= ("\nYour recommended schedule will take an extra " + total + " minutes in breaktime.");
+    console.log(str);
+
+    document.getElementById("emptyP").innerHTML = str;
+  //}
+
+document.getElementById("button").addEventListener('click', mainB());
+//}
 }
